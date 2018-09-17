760bf060b706f105e27cf6b546ad9c2ed427a0a2
Vincent Delacourt <vincent.delacourt@adama-development.com>
2018-09-17 17:46:38 +0800

    add button on progress bar

--- src/components/ui_components/progress_bar/index.tsx
+++ src/components/ui_components/progress_bar/index.tsx
@@ -1,4 +1,4 @@
-import { Grid, Paper, Theme, Typography, withTheme, WithTheme } from '@material-ui/core';
+import { ButtonBase, Grid, Paper, Typography, withTheme, WithTheme } from '@material-ui/core';
 import * as React from 'react';
 import { style } from './style';
 
@@ -11,38 +11,48 @@ export interface ProgressBarProps {
      * The active index to show (first one is zero)
      */
     activeIndex: number;
+    /**
+     * The Function to launch when step is clicked
+     */
+    action?: (index: number) => void;
 }
 
 const ProgressBarBase: React.StatelessComponent<ProgressBarProps & WithTheme> = (props) => {
 
-    const { menuList, activeIndex, theme } = props;
+    const { menuList, activeIndex, action, theme } = props;
+
+
+    const renderMenu = () => {
+        return menuList.map((menuItem, index) => {
+            if (index === activeIndex) {
+                return (
+                    <ButtonBase style={style(theme).buttonBase} onClick={() => action ? action(index) : undefined} >
+                        <Grid key={menuItem} item={true} style={style(theme).activeItemContainer}>
+                            <Paper style={style(theme).activeContainer} elevation={5}>
+                                <Typography variant={'subheading'} color={'inherit'} >{menuItem.toUpperCase()}</Typography>
+                            </Paper>
+                        </Grid >
+                    </ButtonBase>
+                );
+            }
+            return (
+                <ButtonBase style={style(theme).buttonBase} onClick={() => action ? action(index) : undefined} >
+                    <Grid key={menuItem} item={true} style={style(theme).inActiveItemContainer}>
+                        <Paper style={style(theme).inActiveContainer} elevation={0}>
+                            < Typography variant={'subheading'} >{menuItem.toUpperCase()}</Typography>
+                        </Paper>
+                    </Grid >
+                </ButtonBase>
+            );
+        });
+    };
+
 
     return (
         <Grid container={true} style={style(theme).mainContainer} alignItems={'center'}>
-            {renderMenu(menuList, activeIndex, theme)}
+            {renderMenu()}
         </Grid >
     );
-};
-
-const renderMenu = (menuList: string[], activeIndex: number, theme: Theme) => {
-    return menuList.map((menuItem, index) => {
-        if (index === activeIndex) {
-            return (
-                <Grid key={menuItem} item={true} style={style(theme).activeItemContainer}>
-                    <Paper style={style(theme).activeContainer} elevation={5}>
-                        <Typography variant={'subheading'} color={'inherit'} >{menuItem.toUpperCase()}</Typography>
-                    </Paper>
-                </Grid >
-            );
-        }
-        return (
-            <Grid key={menuItem} item={true} style={style(theme).inActiveItemContainer}>
-                <Paper style={style(theme).inActiveContainer} elevation={0}>
-                    < Typography variant={'subheading'} >{menuItem.toUpperCase()}</Typography>
-                </Paper>
-            </Grid >
-        );
-    });
 };
 
 const ProgressBarWithTheme: React.ComponentType<ProgressBarProps> = withTheme()(ProgressBarBase);
--- stories/2_components/1_ui_components/03_progress_bar/index.stories.tsx
+++ stories/2_components/1_ui_components/03_progress_bar/index.stories.tsx
@@ -1,4 +1,5 @@
 
+import { action } from '@storybook/addon-actions';
 import { withInfo } from '@storybook/addon-info';
 import { RenderFunction, storiesOf } from '@storybook/react';
 import * as React from 'react';
@@ -6,31 +7,32 @@ import { ProgressBar } from '../../../../src';
 import { injectTheme } from '../../../decorators';
 
 export default storiesOf('2.1.3 Progress Bar', module)
-  .addDecorator((story: RenderFunction) => {
-    return (
-      injectTheme(story)
-    );
-  })
-  //
-  .add('ProgressBar', withInfo({ source: true })(() => (
-    <ProgressBar
-      activeIndex={0}
-      menuList={['Sourced', 'Applied', 'Interview', 'Hired']}
-    />
-  )))
-  //
-  .add('Progress Bar Middle', (() => (
-    <ProgressBar
-      activeIndex={2}
-      menuList={['Sourced', 'Applied', 'Interview', 'Selection', 'Hired']}
-    />
-  )))
-  //
-  .add('Progress Bar End', (() => (
-    <ProgressBar
-      activeIndex={5}
-      menuList={['Sourced', 'Applied', 'Interview', 'Selection', 'Probation', 'Hired']}
-    />
-  )))
-  //
-  ;
+    .addDecorator((story: RenderFunction) => {
+        return (
+            injectTheme(story)
+        );
+    })
+    //
+    .add('ProgressBar', withInfo({ source: true })(() => (
+        <ProgressBar
+            activeIndex={0}
+            menuList={['Sourced', 'Applied', 'Interview', 'Hired']}
+            action={action('step clicked')}
+        />
+    )))
+    //
+    .add('Progress Bar Middle', (() => (
+        <ProgressBar
+            activeIndex={2}
+            menuList={['Sourced', 'Applied', 'Interview', 'Selection', 'Hired']}
+        />
+    )))
+    //
+    .add('Progress Bar End', (() => (
+        <ProgressBar
+            activeIndex={5}
+            menuList={['Sourced', 'Applied', 'Interview', 'Selection', 'Probation', 'Hired']}
+        />
+    )))
+    //
+    ;
